c76f6e79654186e676184b99b860725b7c55f63e
Adi Levi <adile0194@gmail.com>
2024-02-09 11:28:15 +0200

    UPDATE

---
+++ db/mongo.storage.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Path = require('path');
+const { EventEmitter } = require('events');
+const consts = require('../constants');
+
+const { DB_HOST, DB_USER, DB_PASS } = consts;
+
+class MongoStorage extends EventEmitter {
+  constructor(entity) {
+    super();
+    this.entityName = entity.charAt(0).toLowerCase() + entity.slice(1);
+    this.Model = require(Path.join(__dirname, `../models/${this.entityName}.model.js`));
+    this.connect();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  connect() {
+    const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}`;
+    mongoose
+      .connect(url)
+      .then(() => console.log('connected to MongoDB'))
+      .catch((err) => console.log(`connection error: ${err}`));
+  }
+
+  find() {
+    return this.Model.find({});
+  }
+
+  retrieve(id) {
+    return this.Model.findOne(id);
+  }
+
+  create(data) {
+    return this.Model.create(data);
+  }
+
+  delete(id) {
+    return this.Model.findByIdAndDelete(id);
+  }
+
+  update(id, data) {
+    return this.Model.updateOne({ id }, data);
+  }
+
+}
+
+module.exports = { MongoStorage };
